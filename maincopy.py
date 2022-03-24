074e907f5886b06b2ef1b6c39c2b6a6337660bab
Arun Chithran <achithran@gmail.com>
2022-03-24 16:03:21 +0530

    new lines

--- maincopy.py
+++ maincopy.py
@@ -1,15 +1,1 @@
-# Arun Chithran is editonmg the code ###
-# Here too ##
-
-# Lstibng in here
-
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from Python!"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+#Hrere
