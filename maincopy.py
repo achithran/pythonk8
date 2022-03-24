08b6a1b7ccfdb04c3f45d688d34b5d63dbefe526
Arun Chithran <achithran@gmail.com>
2022-03-24 16:11:47 +0530

    werwer

--- maincopy.py
+++ maincopy.py
@@ -3,13 +3,3 @@
 
 # Lstibng in here
 
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from Python!"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
